4e70c639f597e90aecdeaf7ee058d7e0d810f23d
Ali Almaweri <alialm@umich.edu>
2025-08-22 20:35:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Hussein!"
+    return "Hello from Alk!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
